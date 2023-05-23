3fce4207aec2dd01b3d45ef10715d73546fe5ca7
mauroph1 <122278388+mauroph1@users.noreply.github.com>
2023-05-23 10:29:19 -0300

    testando cores e estilos

--- lib/views/geekhub_view.dart
+++ lib/views/geekhub_view.dart
@@ -13,40 +13,46 @@ class GeekHubHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('GeekHub'),
-      ),
-      body: ListView.builder(
-        itemCount: geekTopics.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              leading: const Icon(Icons.star),
-              title: Text(geekTopics[index]),
-              subtitle: const Text('Tap to learn more'),
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text(geekTopics[index]),
-                      content: Text(
-                          'This is some information about ${geekTopics[index]}.'),
-                      actions: [
-                        TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text('Close'))
-                      ],
+        appBar: AppBar(
+          title: const Text('GeekHub'),
+        ),
+        body: Container(
+          color: Colors.black87,
+          child: ListView.builder(
+            itemCount: geekTopics.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  hoverColor: Colors.yellowAccent,
+                  tileColor: Colors.black87,
+                  textColor: Colors.deepPurpleAccent,
+                  iconColor: Colors.yellowAccent,
+                  leading: const Icon(Icons.star),
+                  title: Text(geekTopics[index]),
+                  subtitle: const Text('Tap to learn more'),
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text(geekTopics[index]),
+                          content: Text(
+                              'This is some information about ${geekTopics[index]}.'),
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('Close'))
+                          ],
+                        );
+                      },
                     );
                   },
-                );
-              },
-            ),
-          );
-        },
-      ),
-    );
+                ),
+              );
+            },
+          ),
+        ));
   }
 }
